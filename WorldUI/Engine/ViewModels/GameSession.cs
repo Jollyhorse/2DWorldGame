e6acf28fe92d0f1141a91698f0f7b0500da27b1b
Brian Burnett <b-burnett@cox.net>
2018-06-28 02:20:58 -0700

    Update GameSession

---
+++ WorldUI/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Brian";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
